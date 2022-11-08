4c3931be8ff5d4445e804b47c9f8b5ad2ce070da
Dogru-Isim <96004353+Dogru-Isim@users.noreply.github.com>
2022-11-08 23:26:20 +0000

    Fixed line changing issues (on backspace)
    Fixed the issue of not going one-line-up on backspace,
    Fixed the issue with deleting the first character.
    Also: Removed the previous fixing "attempts" result: "cursorXPosition". (Oops)

--- main.nim
+++ main.nim
@@ -1,6 +1,8 @@
 import std/[terminal, os, strutils, random, sequtils, parseopt]
 import system
 
+let width = terminalWidth()
+
 proc removeUnfinishedSentence(text: var string) =
   while true:
     if len(text) == 0:
@@ -8,7 +10,7 @@ proc removeUnfinishedSentence(text: var string) =
       quit(0)
     elif text[^1] != '.':
       text = text[0..^2]
-    else:                       #TODO: This is not the best solution since there will be dots in the middle of the sentences (e.g. "1.")
+    else:                       #TODO: This is not the best solution since there will be dots at the middle of a sentence ("1." i.e)
       break
 
 proc getTextLength(): int =
@@ -20,7 +22,7 @@ proc getTextLength(): int =
     of cmdLongOption, cmdShortOption: discard
     of cmdArgument:
       return parseInt(length.key)
-
+  
 #[
   TODO: Get texts from sources on the internet based on their length (preferably the Britannica API)
 
@@ -54,17 +56,37 @@ proc prepare() =
   stdout.write(text)
   setCursorPos(0, 0)
 
+# There is no built-in getCursorPos, 
+# Thanks to: cagyul in https://forum.nim-lang.org/t/9475, I modified it a bit for my needs.
+proc getCursorPos(charsToWrite: string): tuple[x,y: int] =
+   stdout.write "\e[6n"       # get cursor coordinates
+   discard getch()              # ignore \e
+   discard getch()              # ignore [
+   var c = getch()
+   stdout.write "\e[y;xR"     # read y,x values of cursor position
+   while c != ';':
+      result.y = result.y * 10 + (int(c) - int('0'))
+      c = getch()
+   c = getch()
+   while c != 'R':
+      result.x = result.x * 10 + (int(c) - int('0'))
+      c = getch()
+   result.x -= 1
+   result.y -= 1
+   setCursorPos(result.x, result.y)      # place cursor at proper position
+   stdout.write charsToWrite                     # overwrite side-effect text ";xR"
+   setCursorPos(result.x, result.y)      # return cursor to proper position
+
 
 # Get users input char by char, compare it with the text
 proc main(): void =
   let text = getText()  # Choose a text
-  var cursorXPosition: int = 0  # There is no `getCursorPos()` function that I could implement
 
   var i = 0
   while i < len(text):
     try:
       var input = getch()
-
+   
       if toHex($input) == "1B":    # ESCAPE
         eraseScreen()
         quit(0)
@@ -74,16 +96,24 @@ proc main(): void =
       elif $input == $text[i]:     # If input is correct
         stdout.write($input)
         inc i
-        inc cursorXPosition
       elif toHex($input) == "7F":  # If input is backspace
-        dec i
-        stdout.cursorBackward()
-        stdout.write($text[i])
-        stdout.cursorBackward()
+        var chars = $text[i] & $text[i+1] & $text[i+2] & $text[i+3]  # To overwrite getCursorPos's side-effect: ";xR"
+        var cursorPos = getCursorPos(chars)
+        if cursorPos[0] == 0:
+          if cursorPos[1] == 0:  # If cursor is at the top, don't do anything
+            continue
+          else:
+            dec i
+            var yPosToSet = cursorPos[1] - 1
+            setCursorPos(width, yPosToSet)
+        else:
+          dec i
+          stdout.cursorBackward()
+          stdout.write($text[i])
+          stdout.cursorBackward()
       elif $input != $text[i]:      # If input is incorrect
         stdout.styledWrite(fgRed, $text[i])
         inc i
-        inc cursorXPosition
     except:
       quit("Unexpected error", 1)
 
